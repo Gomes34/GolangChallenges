a59e5ab89fdd11aaf92a52bc16a8b5962f11536d
Gomes <gdemonst@gmail.com>
2024-02-15 19:14:15 -0300

    feat: level easy in construction

---
+++ levelEasy/ex002/ex002.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var a, b int = 2, 2
+
+func main() {
+	fmt.Println(a + b)
+}
---
+++ levelEasy/ex003/ex003.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var a = 1
+var b = "1"
+
+func main() {
+
+	fmt.Printf("A = %T\n", a)
+	fmt.Printf("B = %T\n", b)
+}
