0d742be92837bcf9f8378b0d7d3d0c485e3b5305
Gomes <gdemonst@gmail.com>
2024-02-16 15:39:18 -0300

    feat: levelEasy, ex005 done

---
+++ levelEasy/ex005/ex005.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b = 2, 2
+
+	if a+b < 5 {
+		fmt.Println("Seu número é menor que cinco")
+	} else {
+		fmt.Println("Seu número é maior que cinco")
+	}
+}
